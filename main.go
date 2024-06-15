0a08ed652aca536b793da336463ec73f9d297bf4
ErrorSyntax1 <ken522.king@gmail.com>
2024-06-15 11:29:59 +0900

    change main.go

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type City struct {
+	ID          int    `json:"ID,omitempty" db:"ID"`
+	Name        string `json:"name,omitempty" db:"Name"`
+	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	District    string `json:"district,omitempty" db:"District"`
+	Population  int    `json:"population,omitempty" db:"Population"`
+}
 
 func main() {
-	fmt.Println("なろう講習会へようこそ！")
+	argCity := "Tokyo"
+	if len(os.Args) >= 2 {
+		argCity = os.Args[1]
+	}
+
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conf := mysql.Config{
+		User:      os.Getenv("DB_USERNAME"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("DB_HOSTNAME") + ":" + os.Getenv("DB_PORT"),
+		DBName:    os.Getenv("DB_DATABASE"),
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("connected")
+	var city City
+	err = db.Get(&city, "SELECT * FROM city WHERE Name = ?", argCity)
+	if errors.Is(err, sql.ErrNoRows) {
+		log.Printf("no such city Name = '%s'\n", argCity)
+		return
+	}
+	if err != nil {
+		log.Fatalf("DB Error: %s\n", err)
+	}
+	log.Printf("%sの人口は%d人です\n", argCity, city.Population)
 }
